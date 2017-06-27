652d0e66725eb51baf416dcecaa7538750c7a98f
Abhishek <akumar074@gmail.com>
2017-06-27 16:21:16 +0530

    Intent Parser modification

--- intentparser/src/main/java/com/stackroute/swisit/intentparser/IntentParserApplication.java
+++ intentparser/src/main/java/com/stackroute/swisit/intentparser/IntentParserApplication.java
@@ -1,5 +1,5 @@
 package com.stackroute.swisit.intentparser;
-
+/*-----------Importing Liberaries-----------*/
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
+/*-------------Spring Boot Application Main Class--------------*/
 @SpringBootApplication
 public class IntentParserApplication {
 
@@ -20,20 +20,21 @@ public class IntentParserApplication {
 
        SpringApplication.run(IntentParserApplication.class, args);   
     }
-    
+    /*-----------------Resolving Locale-----------------*/
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.US);
         return slr;
     }
+    /*------------------Loading Message Bundles--------------------*/
     @Bean
     public MessageSource messageSource() {
          ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
          messageSource.setBasename("classpath:/messages/messages");
          return messageSource;
     }
-
+    /*-------------Changing Language with User Preference------------*/
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
--- intentparser/src/main/java/com/stackroute/swisit/intentparser/repository/RelationshipRepository.java
+++ intentparser/src/main/java/com/stackroute/swisit/intentparser/repository/RelationshipRepository.java
@@ -12,27 +12,35 @@ import java.util.Map;
 /*------------Repository for Relationship Data of Neo4j DB-----------*/
 @Repository
 public interface RelationshipRepository extends GraphRepository<IndicatorOf> {
+
     /*----------Query to get indicatorOf Relation between Terms and Intents----------*/
     @Query("MATCH ()-[r:indicatorOf]->() RETURN r")
     Iterable<Map<String,Object>> getAllIndicator();
+
     /*----------Query to get counterIndicatorOf Relation between Terms and Intents----------*/
     @Query("MATCH ()-[r:counterIndicatorOf]->() RETURN r")
     Iterable<Map<String,Object>> getAllCounterIndicator();
+
     /*----------Query to get IntentOf Relation between Intents and Domain----------*/
     @Query("MATCH ()-[r:IntentOf]->() RETURN r")
     Iterable<Map<String,Object>> getAllIntentOF();
+
     /*----------Query to get all Relation between Terms and Intents----------*/
     @Query("Match p=(t:Term)-[r]->(i:Intent) RETURN r")
     Iterable<Map<String,Object>> getBothRelationships();
+
     /*----------Query to get all Relations between Terms and Intents----------*/
     @Query("Match p=(t:Term)-[]->(i:Intent)-[]->(d:Intent) RETURN p")
     Iterable<Map<String,Object>> getAllRelationships();
+
     /*----------Query to get all Relations between Terms and Intents as IntentParserResult----------*/
     @Query("Match (t:Term)-[r]->(i:Intent) return t.name AS termName,i.name AS intentName,type(r) AS relName,r.weight AS weight")
     List<Map<String,String>> fetchAllRelationships();
+
     /*----------Query to get Terms Relation between Terms and an Intent----------*/
     @Query("Match (t:Term)-[r]->(i:Intent {name: {intentName}}) return t.name AS termName,i.name AS intentName,type(r) AS relName,r.weight AS weight")
     List<Map<String,String>> getAllTermsRelationOfIntent(@Param("intentName") String intentName);
+
     /*----------Query to create Resulted Relations between URLs and Concepts----------*/
     // @Query("MATCH (w:Document {url:{url}}) MATCH (c:Concept {name:{concept}}) call apoc.create.relationship(w,{intentName},{confidenceScore:{confScore}},c) YIELD rel AS r Return w,r,c")
     @Query("Match (d:Document),(c:Concept) where d.url={url} and c.name={concept} create (d)-[:${intentName}{confidenceScore:{confScore}}]->(c) return d,c ")
