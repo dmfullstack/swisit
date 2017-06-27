293686437d8cc92f42f41100486bb9bc99cf9fbc
Abhishek <akumar074@gmail.com>
2017-06-27 12:50:06 +0530

    Intent Parser modification

--- intentparser/src/main/java/com/stackroute/swisit/intentparser/service/IntentParserService.java
+++ intentparser/src/main/java/com/stackroute/swisit/intentparser/service/IntentParserService.java
@@ -1,18 +1,20 @@
 package com.stackroute.swisit.intentparser.service;
 
+/*------Importing Liberaries-------*/
 import com.stackroute.swisit.intentparser.domain.Intent;
 import com.stackroute.swisit.intentparser.domain.Term;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+/*---------IntentParserService Interface Class---------*/
 public interface IntentParserService {
+
+    /*-------Methods to Interact with Neo4j Database------*/
     List<Intent> getAllIntents();
     List<Term> getAllTerms();
     Iterable<Map<String,Object>> getAllIndicator();
     Iterable<Map<String,Object>> getBothRelationships();
     Iterable<Map<String,Object>> getAllRelationships();
-    List<Map<String,String>> fetchAllRelationships();
     Iterable<Map<String,String>> getAllTermsRelationOfIntent(String intentName);
 }
--- intentparser/src/main/java/com/stackroute/swisit/intentparser/service/IntentParserServiceImpl.java
+++ intentparser/src/main/java/com/stackroute/swisit/intentparser/service/IntentParserServiceImpl.java
@@ -1,5 +1,5 @@
 package com.stackroute.swisit.intentparser.service;
-
+/*---------Importing Liberaries--------*/
 import com.stackroute.swisit.intentparser.domain.Intent;
 import com.stackroute.swisit.intentparser.domain.Term;
 import com.stackroute.swisit.intentparser.repository.IntentRepository;
@@ -12,47 +12,47 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/*---------Implemenentation Class for IntentParserService-------*/
 @Service
 public class IntentParserServiceImpl implements IntentParserService {
 
+    /*---------Autowired Repositories Instances--------*/
     @Autowired
     IntentRepository intentRepository;
-
     @Autowired
     TermRepository termRepository;
-
     @Autowired
     RelationshipRepository relationshipRepository;
 
+    /*----------Method to get All Intent Nodes From Repository----------*/
     @Override
     public List<Intent> getAllIntents() {
         return intentRepository.findIntents();
     }
 
+    /*----------Method to get All Term Nodes From Repository---------*/
     @Override
     public List<Term> getAllTerms() {
         return termRepository.findTerms();
     }
 
+    /*----------Method to get All Indicator Terms From Repository----------*/
     @Override
     public Iterable<Map<String,Object>> getAllIndicator() {
         return relationshipRepository.getAllIndicator();
     }
 
+    /*----------Method to get Indicator and CounterIndicator Relationships from Repository---------*/
     @Override
     public Iterable<Map<String, Object>> getBothRelationships() { return relationshipRepository.getBothRelationships(); }
 
+    /*--------Method to get all relationships between Term and Intent nodes--------*/
     @Override
     public Iterable<Map<String,Object>> getAllRelationships() {
         return relationshipRepository.getAllRelationships();
     }
 
-    @Override
-    public List<Map<String,String>> fetchAllRelationships() {
-        System.out.println(relationshipRepository.fetchAllRelationships().toString());
-        return relationshipRepository.fetchAllRelationships();
-    }
-
+    /*----------Method to get All the Term nodes Related to Intent Node --------*/
     @Override
     public Iterable<Map<String,String>> getAllTermsRelationOfIntent(String intentName) {
         return relationshipRepository.getAllTermsRelationOfIntent(intentName);
