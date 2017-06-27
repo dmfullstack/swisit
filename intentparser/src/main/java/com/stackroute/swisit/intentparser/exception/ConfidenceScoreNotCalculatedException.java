54d61f5ec9011ca6cd37d4af7e279ec23cb0a6ef
Abhishek <akumar074@gmail.com>
2017-06-27 16:12:15 +0530

    Intent Parser modification

--- intentparser/src/main/java/com/stackroute/swisit/intentparser/exception/ConfidenceScoreNotCalculatedException.java
+++ intentparser/src/main/java/com/stackroute/swisit/intentparser/exception/ConfidenceScoreNotCalculatedException.java
@@ -1,7 +1,7 @@
 package com.stackroute.swisit.intentparser.exception;
-/*-------------Custom Exception Han------------*/
+/*-------------Custom Exception Handling Class------------*/
 public class ConfidenceScoreNotCalculatedException extends Exception{
-	
+	/*--------------Exception Handling Method to Show Message-------------*/
 	public ConfidenceScoreNotCalculatedException(String message)
 	{
 	super(message);	
